e5d1437ddb24c61a7f8b2df852f8e9071fe78dec
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-12-02 20:47:06 +0100

    create day 1 first star

--- day01/main.c
+++ day01/main.c
@@ -2,23 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DIAL_SIZE 100
+#define START_POSITION 50
+
 int main() {
     int num_lines;
+    int position = START_POSITION;
+    int count = 0;
     char **lines = read_lines_from_file("input.txt", &num_lines);
+
     if (lines == NULL) {
         printf("Error reading file\n");
         return 1;
     }
 
-    printf("Read %d lines:\n", num_lines);
     for (int i = 0; i < num_lines; i++) {
-        printf("Line %d: %s\n", i + 1, lines[i]);
+        char direction = lines[i][0];
+        int steps = atoi(lines[i] + 1);
+        if (direction == 'L') {
+            position = (position - steps) % DIAL_SIZE;
+        } else if (direction == 'R') {
+            position = (position + steps) % DIAL_SIZE;
+        }
+        if (position == 0) {
+            count++;
+        }
     }
 
     for (int i = 0; i < num_lines; i++) {
         free(lines[i]);
     }
     free(lines);
+
+    printf("%d\n", count);
 
     return 0;
 }
