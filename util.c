ac4a56e255c7b74e4125d5aa26dfb196be4ff42b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-12-03 02:11:12 +0100

    add new read comma separated strings from file method

--- util.c
+++ util.c
@@ -54,3 +54,67 @@ cleanup:
     *num_lines = 0;
     return NULL;
 }
+
+char **read_comma_separated_strings_from_file(const char *filename,
+                                              int *num_strings) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        *num_strings = 0;
+        return NULL;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t result = getline(&line, &len, file);
+    if (result > 0 && line[result - 1] == '\n') {
+        line[result - 1] = '\0';
+    }
+
+    if (result == -1) {
+        free(line);
+        fclose(file);
+        *num_strings = 0;
+        return NULL;
+    }
+
+    int capacity = 16;
+    int count = 0;
+    char **strings = malloc(capacity * sizeof(char *));
+    if (strings == NULL) {
+        free(line);
+        fclose(file);
+        *num_strings = 0;
+        return NULL;
+    };
+
+    char *token = strtok(line, ",");
+    while (token) {
+        strings[count] = strdup(token);
+        count++;
+
+        if (count >= capacity) {
+            capacity *= 2;
+            char **temp = realloc(strings, capacity * sizeof(char *));
+            if (temp == NULL) {
+                goto cleanup;
+            }
+            strings = temp;
+        }
+        token = strtok(NULL, ",");
+    }
+
+    free(line);
+    fclose(file);
+    *num_strings = count;
+    return strings;
+
+cleanup:
+    for (int i = 0; i < count; i++) {
+        free(strings[i]);
+    }
+    free(strings);
+    free(line);
+    fclose(file);
+    *num_strings = 0;
+    return NULL;
+}
--- util.h
+++ util.h
@@ -2,5 +2,6 @@
 #define UTIL_H
 
 char **read_lines_from_file(const char *filename, int *num_lines);
+char **read_comma_separated_strings_from_file(const char *filename, int *num_strings);
 
 #endif
