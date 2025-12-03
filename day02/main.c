b6ff3fe1f386329db92be4f7bb3758ed390d5093
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-12-03 02:40:30 +0100

    fix code stuffs for day2

--- day02/main.c
+++ day02/main.c
@@ -19,12 +19,12 @@ int main() {
 
     for (int i = 0; i < num_strings; i++) {
         char *token = strtok(strings[i], "-");
-        int part1 = atoi(strdup(token));
+        int part1 = atoi(token);
         token = strtok(NULL, "-");
-        int part2 = atoi(strdup(token));
-        for (int i = part1; i < part2; i++) {
-            char *str;
-            sprintf(str, "%d", i);
+        int part2 = atoi(token);
+        for (int j = part1; j <= part2; j++) {
+            char *str = malloc(20);
+            sprintf(str, "%d", j);
             int len = strlen(str);
             if (len % 2 == 0) {
                 char *first = malloc(len / 2 + 1);
@@ -32,22 +32,29 @@ int main() {
                 if (!first || !second) {
                     free(first);
                     free(second);
+                    free(str);
                     return 1;
                 }
                 memcpy(first, str, len / 2);
                 first[len / 2] = '\0';
                 memcpy(second, str + len / 2, len - len / 2);
-                first[len - len / 2] = '\0';
+                second[len - len / 2] = '\0';
 
                 if (atoi(first) == atoi(second)) {
-                    sum += i;
+                    sum += j;
                 }
                 free(first);
                 free(second);
+                free(str);
             }
         }
     }
     printf("Part 1: %d\n", sum);
+
+    for (int i = 0; i < num_strings; i++) {
+        free(strings[i]);
+    }
+    free(strings);
 
     return 0;
 }
